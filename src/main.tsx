452c9276639f8a176f9deb2c96ce394772f09646
Charlie Wilson <charliework@Mac.communityfibre.co.uk>
2026-07-07 21:17:39 +0100

    Adopt UIScene lifecycle for iOS app

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { Capacitor } from "@capacitor/core";
 import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./index.css";
@@ -6,6 +7,6 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
 	<>
 		<App />
-		<Analytics />
+		{!Capacitor.isNativePlatform() && <Analytics />}
 	</>
 );

7af15da03bd1ed184af7a578ea884a19f81ffd7b
Charlie Wilson <charliework@Charlies-MacBook-Air-2.local>
2026-07-09 12:40:55 +0100

    Add remaining App Clip target files and entitlements

---
+++ ios/App/AppClip/AppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClipApp.swift
+//  AppClip
+//
+//  Created by Charlie Wilson on 09/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct AppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
